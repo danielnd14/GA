dda1680a0c20db613c12dec9f83325d9c6f554cb
Daniel Santos <danielnd14@gmail.com>
2019-05-16 10:26:18 -0300

    inserido comentario justificando a falta de funcionamento do problema da funçao Schaffer

--- src/com/github/danielnd14/SchafferProblemMain.java
+++ src/com/github/danielnd14/SchafferProblemMain.java
@@ -5,6 +5,7 @@ import com.github.danielnd14.schaffersf6Problem.SchafferPopulationBuilder;
 
 public class SchafferProblemMain {
 	public static void main(String[] args) {
+		//representaçao do problema ainda nao implementada, portando o codigo abaixo nao funciona
 		var pop = new SchafferPopulationBuilder()
 				.withBinaryTournamentSelection()
 				.withRankMergeStratey()
